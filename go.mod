95b9dc797c8d42625b1bd49b3a9af182dd9f1117
gambtho <Thomas.Gamble@microsoft.com>
2023-09-27 17:17:43 +0000

    initial commit

---
+++ go.mod
@@ -0,0 +1,22 @@
+module helloworld
+
+go 1.19
+
+require (
+	google.golang.org/grpc v1.57.0
+	google.golang.org/grpc/examples v0.0.0-20230927150341-c6264a9f9015
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/stretchr/testify v1.8.3 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+)
+
+require (
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+)
---
+++ greeter_client/main.go
@@ -0,0 +1,81 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+// Package main implements a client for Greeter service.
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	grpc_retry "github.com/grpc-ecosystem/go-grpc-middleware/retry"
+	// "google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/credentials/insecure"
+	pb "google.golang.org/grpc/examples/helloworld/helloworld"
+)
+
+const (
+	defaultName = "world"
+)
+
+var (
+	addr = flag.String("addr", "localhost:50051", "the address to connect to")
+	name = flag.String("name", defaultName, "Name to greet")
+)
+
+func main() {
+	flag.Parse()
+	// Set up a connection to the server.
+
+	opts := []grpc_retry.CallOption{
+		grpc_retry.WithMax(5),
+		grpc_retry.WithPerRetryTimeout(time.Second*5),
+	}
+	conn, err := grpc.DialContext(context.Background(),*addr, 
+	grpc.WithTransportCredentials(insecure.NewCredentials()),
+	grpc.WithUnaryInterceptor(grpc_retry.UnaryClientInterceptor(opts...)),
+	// grpc.WithKeepaliveParams(keepalive.ClientParameters{}),
+	)
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+	
+	count := 0
+	for {
+		log.Print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+		log.Printf("New request from client: %d", count)
+		count++
+
+		duration := time.Now().Add(45 * time.Second)
+		ctx, _ := context.WithDeadline(context.Background(), duration)
+		r, err := c.SayHello(ctx, &pb.HelloRequest{Name: *name})
+		if err != nil {
+			log.Printf("could not greet: %v", err)
+		}
+		log.Printf("Greeting: %s", r.GetMessage())
+
+		log.Print("Sleeping for 5 seconds")
+		log.Print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+		time.Sleep(time.Second*5)
+	}
+}
